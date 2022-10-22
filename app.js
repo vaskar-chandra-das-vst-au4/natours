bf74978c5ea210db22ea704556f5281afc5a72e6
Vaskar Chandra Das <vcdas123@gmail.com>
2022-10-22 10:18:03 +0530

    Tested CORS

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //~ Or we may allow only certain api url to be available to all for that we need to specify cors() middleware there.
 //@ Like app.use('/api/v1/tours', cors() , tourRouter);
-// app.use(cors());
+app.use(cors());
 
 //! GET and POST are called simple requests which are now allowed using above code to other domains.
 
@@ -82,7 +82,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //! so basically when we get one of these option requests on our server we then need to send back the same access-control-allow-origin header . And this way browser will then know that the actual request and in this case a delete request is safe to perform and then execute the actual delete request.
 //~ and this app.options is just like other app.get or app.delete requests. "*" means for all non-simple requests route them to our api using cors().
-// app.options('*', cors());
+app.options('*', cors());
 //@ For specific routes - app.options('/api/tour/:id', cors() )
 
 //! Serve static files -
